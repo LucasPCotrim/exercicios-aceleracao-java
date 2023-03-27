b9ad4843155ffd93876a69a0513bca9f96733f00
Lucas Pereira Cotrim <lucaspcotrim@gmail.com>
2023-03-27 17:57:25 -0300

    feat: exercicio 6 - Ola Spring

---
+++ ola-spring/src/main/java/com/example/olaspring/OlaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.olaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlaSpringApplication.class, args);
+	}
+
+}
